5897521cae7dfca529e239a5a50192f7a3ab24fd
Ryan Michael <77926344+ryan-m-3@users.noreply.github.com>
2021-02-16 22:11:16 +0000

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,10 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void){
-    //get user to input two numbers
-    int x = get_int("Enter a number ");
-    int y = get_int("Enter another number ");
-    //print the answer to adding the two numbers the user inputted
-    printf("The answer is, %i\n", x + y);
+int main(void)
+{
+    int answer = get_int("What's your name? ");
+    printf("hello, %i\n", answer);
 }
+
